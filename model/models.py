63aa6e4dd24c1484834fae7c0fab0028a16da62b
omerAJ <23100050@lums.edu.pk>
2024-02-06 17:08:04 +0500

    .

--- model/models.py
+++ model/models.py
@@ -70,6 +70,7 @@ class STSSL(nn.Module):
         l3 = self.spatial_loss(z1, z2)
         sep_loss.append(l3.item())
         loss += loss_weights[2] * l3 
+        print("predLoss: ", l1.item(), "temporalLoss: ", l2.item(), "spatialLoss: ", l3.item())
         return loss, sep_loss
 
     def pred_loss(self, z1, z2, y_true, scaler):
