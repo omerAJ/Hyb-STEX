8b5e922c8078b5195ce3620d8461e2aeba034357
omerAJ <23100050@lums.edu.pk>
2024-06-10 15:48:01 +0500

    evaluateLosses

--- lib/metrics.py
+++ lib/metrics.py
@@ -6,9 +6,17 @@ def mae_torch(pred, true, mask_value=None):
         mask = torch.gt(true, mask_value)
         # nodesMasked=mask[mask==True].shape[0]
         # print("total nodes masked", nodesMasked, "/4096",  "nodes on average masked in each sample: ", nodesMasked/true.shape[0])
+        inv_mask = ~mask
+
+        masked_count = mask.sum().item()
+        unmasked_count = inv_mask.sum().item()
+        # print(f"mask.shape: {mask.shape}, pred.shape: {pred.shape}, true.shape: {true.shape}. ~mask.shape: {inv_mask.shape}")
+        unmasked_pred = torch.masked_select(pred, ~mask)
+        unmasked_true = torch.masked_select(true, ~mask)
         pred = torch.masked_select(pred, mask)
         true = torch.masked_select(true, mask)
-    return torch.mean(torch.abs(true-pred))
+        
+    return torch.mean(torch.abs(true-pred)), torch.mean(torch.abs(unmasked_true-unmasked_pred)), masked_count, unmasked_count
 
 def mape_torch(pred, true, mask_value=None):
     if mask_value != None:
@@ -21,7 +29,7 @@ def mae_np(pred, true, mask_value=None):
     if mask_value != None:
         
         mask = np.where(true > (mask_value), True, False)  ## True where condition met false elsewhere
-        print("mask.shape: ", mask.shape)
+        # print("mask.shape: ", mask.shape)
         true = true[mask]
         pred = pred[mask]
     return np.mean(np.absolute(pred-true))
@@ -40,10 +48,12 @@ def test_metrics(pred, true, mask1=5, mask2=5):
         mae  = mae_np(pred, true, mask1)
         mape = mape_np(pred, true, mask2)
     elif type(pred) == torch.Tensor:
-        mae  = mae_torch(pred, true, mask1).item()
+        mae, masked_mae, masked_count, unmasked_count  = mae_torch(pred, true, mask1)
+        mae = mae.item()
+        masked_mae = masked_mae.item()
         mape = mape_torch(pred, true, mask2).item()
     else:
         raise TypeError
-    return mae, mape
+    return mae, mape, masked_mae, masked_count, unmasked_count
 
 
