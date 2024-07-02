fbf7fb20fc89105c79b170c018dbd8dbfa523cd6
omerAJ <23100050@lums.edu.pk>
2024-07-02 17:16:37 +0500

    .

--- run_experiments_graphInit.bat
+++ run_experiments_graphInit.bat
@@ -4,17 +4,22 @@ setlocal enabledelayedexpansion
 call activate i-jepaVENV
 TIMEOUT 3
 
-python main.py -g "8_neighbours" -c "fullyAttentionalEncoder (3xsa 3xff all skip no ln)(yes pos_embed (correct))" -cf configs/NYCBike1.yaml -pef True 
+python main.py -g batched_cheb -c "fixed encoder, learning weights, only view1, with softmax" -s 2 -fe True
 echo Experiment completed: Ks = 1
 TIMEOUT 3
 
-python main.py -g "8_neighbours" -c "fullyAttentionalEncoder (3xsa 3xff all skip no ln)(yes pos_embed (correct))" -cf configs/NYCBike2.yaml -pef True 
+python main.py -g "batched_cheb" -c "fixed encoder, learning weights, only view1, with softmax" -fe True -s 3
 echo Experiment completed: Ks = 1
 TIMEOUT 3
 
-python main.py -g "8_neighbours" -c "fullyAttentionalEncoder (3xsa 3xff all skip no ln)(yes pos_embed (correct))" -cf configs/NYCTaxi.yaml -pef True 
+python main.py -g "batched_cheb" -c "fixed encoder, learning weights, only view1, with softmax" -fe True -s 4
 echo Experiment completed: Ks = 1
 TIMEOUT 3
+
+python main.py -g "batched_cheb" -c "fixed encoder, learning weights, only view1, with softmax" -fe True -s 5
+echo Experiment completed: Ks = 1
+TIMEOUT 3
+
 
 echo All experiments completed.
 TIMEOUT 99999
