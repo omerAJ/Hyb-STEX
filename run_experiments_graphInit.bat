409156f3446350973b83bc9945eca03a3dcb01bb
ShahmirAltamash <shahmir2001@gmail.com>
2024-07-23 10:37:48 +0500

    .

--- run_experiments_graphInit.bat
+++ run_experiments_graphInit.bat
@@ -1,24 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-call activate ST-SSL
+call activate i-jepaVENV
 TIMEOUT 3
 
-
-
-REM Loop through values 1 to 3 for the -s parameter
-FOR /L %%G IN (1,1,3) DO (
-    python main.py -c "evl (cheating)" -s %%G -cf configs/NYCBike1.yaml
-    echo Experiment completed: Ks = %%G
-    TIMEOUT /T 3 /NOBREAK
-)
-
-REM Loop through values 1 to 3 for the -s parameter
-FOR /L %%G IN (1,1,3) DO (
-    python main.py -c "evl (cheating)" -s %%G -cf configs/NYCBike2.yaml
-    echo Experiment completed: Ks = %%G
-    TIMEOUT /T 3 /NOBREAK
-)
 
 REM Loop through values 1 to 3 for the -s parameter
 FOR /L %%G IN (1,1,3) DO (
