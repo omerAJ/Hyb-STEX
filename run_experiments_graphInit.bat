ca89123feb52cc0caab96037e3eeeb316a45a21b
ShahmirAltamash <shahmir2001@gmail.com>
2024-07-25 11:58:31 +0500

    .

--- run_experiments_graphInit.bat
+++ run_experiments_graphInit.bat
@@ -7,7 +7,7 @@ TIMEOUT 3
 
 REM Loop through values 1 to 3 for the -s parameter
 FOR /L %%G IN (1,1,3) DO (
-    python main.py -c "evl (cheating)" -s %%G -cf configs/BJTaxi.yaml
+    python main.py -c "evl (cheating) noisy_gt 10, 5" -s %%G -cf configs/NYCTaxi.yaml
     echo Experiment completed: Ks = %%G
     TIMEOUT /T 3 /NOBREAK
 )
