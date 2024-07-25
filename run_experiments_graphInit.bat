e2bb673f5a0e11009b2e2cc5060766851570cdc3
omerAJ <23100050@lums.edu.pk>
2024-07-25 13:24:11 +0500

    .

--- run_experiments_graphInit.bat
+++ run_experiments_graphInit.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-call activate i-jepaVENV
+call activate ST-SSL
 TIMEOUT 3
 
 
 REM Loop through values 1 to 3 for the -s parameter
 FOR /L %%G IN (1,1,3) DO (
-    python main.py -c "evl (cheating) noisy_gt 10, 5" -s %%G -cf configs/NYCTaxi.yaml
+    python main.py -c "evl (cheating) noisy_gt 5, 2.5" -s %%G -cf configs/NYCTaxi.yaml
     echo Experiment completed: Ks = %%G
     TIMEOUT /T 3 /NOBREAK
 )
