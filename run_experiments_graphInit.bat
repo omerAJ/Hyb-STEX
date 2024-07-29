b928d6816cd05e68c4d89a9e451bfeabf7dbc427
omerAJ <23100050@lums.edu.pk>
2024-07-29 15:11:08 +0500

    90 percentile evs

--- run_experiments_graphInit.bat
+++ run_experiments_graphInit.bat
@@ -7,7 +7,7 @@ TIMEOUT 3
 
 REM Loop through values 1 to 3 for the -s parameter
 FOR /L %%G IN (1,1,3) DO (
-    python main.py -c "evl (cheating) noisy_gt 2, 1" -s %%G -cf configs/NYCTaxi.yaml
+    python main.py -c "evl (cheating) 90 percentile evs" -s %%G -cf configs/NYCBike2.yaml
     echo Experiment completed: Ks = %%G
     TIMEOUT /T 3 /NOBREAK
 )
